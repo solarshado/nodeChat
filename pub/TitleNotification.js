7d5fd6e345a80f89230081fdc92412af7f98de28
Adrian Todd <solarshado@gmail.com>
2014-09-18 12:49:50 -0400

    More features for TitleNotification.js; related to issue #1

--- pub/TitleNotification.js
+++ pub/TitleNotification.js
@@ -3,13 +3,17 @@ var TitleNotification = TitleNotification || (function () {
 		oldTitle,
 		ticInterval = 350,
 		isTock = false,
+		useStar = false,
 		UNDERBAR = "__",
-		OVERBAR = String.fromCharCode(0x203E, 0x203E);
+		OVERBAR = String.fromCharCode(0x203E, 0x203E)
+		STAR = "*"
+		;
 
 	function tic() {
 		isTock = !isTock;
-		var afix = isTock ? UNDERBAR : OVERBAR;
-		document.title = afix + " " + oldTitle + " " + afix;
+		var afix = isTock ? UNDERBAR : OVERBAR,
+			innerAfix = useStar ? STAR : "";
+		document.title = afix + innerAfix + " " + oldTitle + " " + innerAfix + afix;
 	}
 
 	var obj = {
@@ -18,11 +22,13 @@ var TitleNotification = TitleNotification || (function () {
 			oldTitle = document.title;
 			timerId = setInterval(tic,ticInterval);
 		},
+		enableStar: function() { useStar = true; },
 		disable: function() {
 			if(!timerId) return; // already stopped
 			clearInterval(timerId);
 			document.title = oldTitle;
 			timerId = false;
+			useStar = false;
 		}
 	}
 	return obj;
